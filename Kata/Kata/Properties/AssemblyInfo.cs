36301f02173927e2a1e3ee7d97d4b691befa2f8d
TitanDarren <sqz777isme@gmail.com>
2017-07-28 15:34:13 +0800

    initial

---
+++ Kata/Kata/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kata")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Titansoft")]
+[assembly: AssemblyProduct("Kata")]
+[assembly: AssemblyCopyright("Copyright © Titansoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42a14163-84f7-4fb5-918b-858e9ee6094a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
